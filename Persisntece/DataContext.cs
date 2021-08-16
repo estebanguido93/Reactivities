1b07c4e1d313fb3de761492f2fda750efd96f736
estebanguido93 <estebanguido93@gmail.com>
2021-08-16 14:09:52 -0300

    Primer commit

---
+++ Persisntece/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persisntece
+{
+    public class DataContext : DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
